0a0d29fc39a14f4e18c8ae532c78519a73d4e82f
medvedium <mumbles2606@gmail.com>
2022-11-04 11:17:27 +0300

    Added link to logo

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -2,14 +2,15 @@ import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import AppHeaderNav from "../app-header-nav/app-header-nav";
 import HeaderLogin from "../header-login/header-login";
 import styles from "./app-header.module.css";
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
   return (
     <header className={`${styles.header} pt-4 pb-4`}>
       <AppHeaderNav />
-      <div className={styles.logoWrap}>
+      <Link to={"/"} className={styles.logoWrap}>
         <Logo />
-      </div>
+      </Link>
       <HeaderLogin />
     </header>
   );
