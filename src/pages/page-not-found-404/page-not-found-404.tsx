1b7389c9dab50e3155abb99b2109a0b3c2a970ce
medvedium <mumbles2606@gmail.com>
2022-12-10 11:54:45 +0300

    Updates

--- src/pages/page-not-found-404/page-not-found-404.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageNotFound404Page = () => {
-  return <div>404</div>;
-};
-
-export default PageNotFound404Page;
--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const _BUN = "bun";
-export const _SAUCE = "sauce";
-export const _MAIN = "main";
-
-export const _WS_URL = "wss://norma.nomoreparties.space/orders";
